177c0ed6efd2e7cf4ddf5d3a2789f7d2ca1fe1ad
The-Yearly <theyearlyclub@gmail.com>
2025-02-15 01:09:10 +0530

    made it more responsive

--- app/page.tsx
+++ app/page.tsx
@@ -8,80 +8,78 @@ export default function Home(){
   const [data,setData]=useState<FoodTemp[]|null>(null)
   const [filteredData,setFilter]=useState<FoodTemp[]|null>(null)
   const [search,setSearch]=useState("")
+  /*'https://cafe-menu-system-backend.vercel.app/users/0*/
   useEffect(()=>{const fetchdata=async()=>{
     let link="";
     if(route==0){
-      link="https://cafe-menu-system-backend.vercel.app/users/0"
+      link="http://localhost:8000/users/0"
+      setSearch("") 
+      setFilter(null) 
     }
     else if(route==1){
-      link="https://cafe-menu-system-backend.vercel.app/users/1"
+      link="http://localhost:8000/users/1"
     }
     else if(route==2){
-      link="https://cafe-menu-system-backend.vercel.app/users/2"
+      link="http://localhost:8000/users/2"
     }
     else if(route==3){
-      link="https://cafe-menu-system-backend.vercel.app/users/3"
+      link="http://localhost:8000/users/3"
     }
     else if(route==4){
-      link="https://cafe-menu-system-backend.vercel.app/users/4"
+      link="http://localhost:8000/users/4"
     } 
-    console.log(link)
     const res=await fetch(link)
     setData(await res.json()) 
   }
   fetchdata()},[route])
-  function Search(val:string){
+  useEffect(()=>{const Search=()=>{
     if(data!=null){
-      const a=data.filter(food=>food.name.toLowerCase().includes(val.toLocaleLowerCase()))
+      const a=data.filter(food=>food.name.toLowerCase().includes(search.toLowerCase()))
+      console.log(a)
       setFilter(a)
   }
   }
+  Search()},[search])
   function gotInput(event:React.ChangeEvent<HTMLInputElement>){
     if(event.target.value!=""){
       setSearch(event.target.value)
-      Search(event.target.value)
     }else{
       setSearch("")
-      Search(event.target.value)
     }
   }
     if(data!=null){
+      console.log(route)
+      console.log(data,filteredData)
     const topfood=data.slice(0,5);
-    console.log(topfood)
-
     return(
       <>
         <div>
-          <Image src="https://www.apjtours.com/admin/assets/img/blog/Best-Cafes-In-Gurgaon-1200x675.jpg" alt="Cafe Image" width={1000} height={1000} className="absolute top-[2vw] left-[27vw] h-auto w-[44vw] rounded-[3vw]"/>
-          <p className="absolute top-[19vw] left-[30vw] font-courier text-[3vw] text-white ">Summer Time Cafe</p>
-          <p className="absolute top-[27vw] font-times left-[47vw] text-[2vw] text-gray-500">Menu</p>
-          <input className="absolute h-[3vw] rounded-[2vw] top-[30vw] left-[27vw] w-[44vw] bg-gray-100 p-[1vw] text-[1vw]" placeholder="Search" value={search} onChange={gotInput}/>
-          <div className="absolute top-[34vw] left-[27vw] h-[3vw] w-[44vw] flex border-[0.1vw] rounded-[1vw] border-black">
-            <div className="relative flex left-[5vw]">
-            <button className="pl-[3vw] font-times text-[1vw]" onClick={()=>setRoute(0)}><p className={route==0?"border-b-[0.2vw] rounded-[0.2vw] pb-[0.1vw]  border-green-600":"border-[0px]"}>Best Sellers</p></button>
-            <button className="pl-[3vw] font-times text-[1vw]" onClick={()=>setRoute(1)}><p className={route==1?"border-b-[0.2vw] rounded-[0.2vw] pb-[0.1vw]  border-green-600":"border-[0px]"}>Foods</p></button>
-            <button className="pl-[3vw] font-times text-[1vw]" onClick={()=>setRoute(2)}><p className={route==2?"border-b-[0.2vw] rounded-[0.2vw] pb-[0.1vw]  border-green-600":"border-[0px]"}>Drinks</p></button>
-            <button className="pl-[3vw] font-times text-[1vw]" onClick={()=>setRoute(3)}><p className={route==3?"border-b-[0.2vw] rounded-[0.2vw] pb-[0.1vw]  border-green-600":"border-[0px]"}>Snacks</p></button>
-            <button className="pl-[3vw] font-times text-[1vw]" onClick={()=>setRoute(4)}><p className={route==4?"border-b-[0.2vw] rounded-[0.2vw] pb-[0.1vw]  border-green-600":"border-[0px]"}>Desserts</p></button> 
+          <Image src="https://www.apjtours.com/admin/assets/img/blog/Best-Cafes-In-Gurgaon-1200x675.jpg" alt="Cafe Image" width={1000} height={1000} className="absolute 2xl:top-[2vw] 2xl:left-[27vw] 2xl:h-auto 2xl:w-[44vw] 2xl:rounded-[3vw] lg:top-[3vw] lg:left-[21vw] lg:h-auto lg:w-[60vw] md:rounded-[3vw] md:top-[4vw] md:left-[12vw] md:w-[75vw] lg:rounded-[3vw]"/>
+          <p className="absolute 2xl:top-[19vw] 2xl:left-[30vw] 2xl:text-[3vw] font-courier text-white lg:top-[28vw] lg:left-[24vw] lg:text-[4vw] md:top-[35vw] md:left-[15vw] md:text-[5vw]">Summer Time Cafe</p>
+          <input className="absolute 2xl:h-[3vw] 2xl:rounded-[2vw] 2xl:top-[30vw] 2xl:left-[27vw] 2xl:w-[44vw] lg:h-[4vw] lg:rounded-[2vw] lg:top-[42vw] lg:left-[21vw] lg:w-[60vw] md:rounded-[2vw] md:h-[5vw] md:top-[54vw] md:left-[12vw] md:w-[75vw] bg-white p-[1vw] " placeholder="Search" value={search} onChange={gotInput}/>
+          <p className="absolute 2xl:top-[27vw] font-times lg:left-[48vw] 2xl:text-[2vw] lg:top-[38vw] 2xl:left-[47vw] lg:text-[2.5vw] md:top-[48vw] md:left-[45vw] md:text-[3.5vw] text-gray-500">Menu</p>
+          <div className="absolute 2xl:top-[34vw] 2xl:left-[27vw] 2xl:h-[3vw] 2xl:w-[44vw] flex border-[0.1vw] 2xl:rounded-[1vw] 2xl:flex 2xl:border-[0.1vw] lg:left-[21vw] lg:top-[47vw] lg:h-[5vw] lg:w-[60vw] lg:rounded-[2vw] md:left-[12vw] md:top-[61vw] md:h-[7vw] md:w-[75vw] md:rounded-[2vw] border-black">
+            <div className="relative flex z-10 2xl:left-[5vw] lg:left-[1vw] ">
+            <button className="2xl:pl-[3vw] font-times 2xl:text-[1vw] lg:pl-[5vw] md:pl-[7vw]" onClick={()=>setRoute(0)}><p className={route==0?"2xl:border-b-[0.2vw] 2xl:rounded-[0.2vw] 2xl:pb-[0.1vw] border-green-600 lg:border-b-[0.3vw] lg:rounded-[0.35vw] lg:pb-[0.3vw] md:border-b-[0.5vw] md:rounded-[0.5vw] md:pb-[0.3vw]":"border-[0px]"}>Starters</p></button>
+            <button className="2xl:pl-[3vw] font-times 2xl:text-[1vw] lg:pl-[5vw] md:pl-[7vw]" onClick={()=>setRoute(1)}><p className={route==1?"2xl:border-b-[0.2vw] 2xl:rounded-[0.2vw] 2xl:pb-[0.1vw] border-green-600 lg:border-b-[0.3vw] lg:rounded-[0.35vw] lg:pb-[0.3vw] md:border-b-[0.5vw] md:rounded-[0.5vw] md:pb-[0.3vw]":"border-[0px]"}>Salads</p></button>
+            <button className="2xl:pl-[3vw] font-times 2xl:text-[1vw] lg:pl-[5vw] md:pl-[7vw]" onClick={()=>setRoute(2)}><p className={route==2?"2xl:border-b-[0.2vw] 2xl:rounded-[0.2vw] 2xl:pb-[0.1vw] border-green-600 lg:border-b-[0.3vw] lg:rounded-[0.35vw] lg:pb-[0.3vw] md:border-b-[0.5vw] md:rounded-[0.5vw] md:pb-[0.3vw]":"border-[0px]"}>Main Course</p></button>
+            <button className="2xl:pl-[3vw] font-times 2xl:text-[1vw] lg:pl-[5vw] md:pl-[7vw]" onClick={()=>setRoute(3)}><p className={route==3?"2xl:border-b-[0.2vw] 2xl:rounded-[0.2vw] 2xl:pb-[0.1vw] border-green-600 lg:border-b-[0.3vw] lg:rounded-[0.35vw] lg:pb-[0.3vw] md:border-b-[0.5vw] md:rounded-[0.5vw] md:pb-[0.3vw]":"border-[0px]"}>Drinks</p></button>
+            <button className="2xl:pl-[3vw] font-times 2xl:text-[1vw] lg:pl-[5vw] md:pl-[7vw]" onClick={()=>setRoute(4)}><p className={route==4?"2xl:border-b-[0.2vw] 2xl:rounded-[0.2vw] 2xl:pb-[0.1vw] border-green-600 lg:border-b-[0.3vw] lg:rounded-[0.35vw] lg:pb-[0.3vw] md:border-b-[0.5vw] md:rounded-[0.5vw] md:pb-[0.3vw]":"border-[0px]"}>Desserts</p></button> 
             </div >
           </div>
         </div>
-        <div className="absolute top-[40vw] left-[29vw]">
-              <ul className="absolute left-[-2vw] grid grid-cols-5 gap-[1vw] w-[45vw] max-h-[150vw]">
-                  {topfood.map((food)=><li key={food.user_id}><Image className="rounded-[4vw] w-[7vw] h-auto" src={food.user_userPic} width={500} height={500} alt="Food Item"/></li>)}
-              </ul>
-              <ul className="absolute top-[9vw]">
-                  {(filteredData??data).map((food)=><li className="pb-[2vw]" key={food.user_id}>
-                  <div className="bg-gray-100 flex items-center justify-center rounded-[2vw] w-[40vw] min-h-[18vw] h-auto p-4">
-                    <div className="absolute  h-[14vw] w-[35vw] rounded-[1vw] bg-white p-4">
-                              <Image className="absolute top-[2vw] left-[1.5vw] w-[9vw] h-auto rounded-[2vw]" alt={"Food Item"+food.user_id} width={1000} height={1000} src={food.user_userPic}/>
-                              <p className="absolute text-gray-500 text-[2vw] left-[13vw] top-[3vw]">{food.name}</p>
-                              <p  className="absolute text-gray-500 font-times top-[6vw] text-[0.9vw] w-[20vw] left-[13vw] line-clamp-3 border-black">{food.user_bio}</p>
-                              <AddButton/>
-                          </div>
-                      </div>
-                  </li>)}
-              </ul>
+        <div className="absolute z-0 2xl:top-[33vw] 2xl:left-[27vw] lg:top-[46vw] lg:left-[21vw] md:top-[63vw] md:left-[12vw]">
+              <div className="grid grid-cols-3 2xl:gap-y-[3vw] 2xl:gap-5 2xl:w-[45vw] lg:gap-y-[5vw] lg:gap-5 lg:w-[60vw] md:gap-y-[5vw] md:gap-10  md:w-[70vw]">
+                  {(filteredData??data).map((food)=>
+                  <div className="relative shadow-lg bg-white flex items-center justify-center 2xl:top-[9vw] 2xl:rounded-[2vw] 2xl:w-[14vw] 2xl:min-h-[17vw] 2xl:p-4 h-auto lg:p-8 lg:top-[9vw] lg:rounded-[2vw] lg:w-[18vw] lg:min-h-[23vw] md:p-8 md:top-[9vw] md:rounded-[2vw] md:w-[24vw] md:min-h-[30vw]" key={food.user_id}>
+                      <Image className="absolute top-[-2vw] 2xl:w-[9vw] h-auto rounded-[10vw] lg:w-[11vw] md:w-[14vw]" alt={"Food Item"+food.user_id} width={1000} height={1000} src={food.user_userPic}/>
+                      <p className="absolute text-center text-black font-courier 2xl:text-[1.5vw] break-words  2xl:w-[10vw] 2xl:top-[7vw] lg:text-[2vw] lg:w-[15vw] lg:top-[10vw]  md:text-[2.5vw] md:w-[20vw] md:top-[12vw]">{food.name}</p>
+                      <p  className="absolute z-10 text-center text-gray-500 font-times 2xl:top-[9.2vw] 2xl:text-[0.9vw] 2xl:w-[10vw] line-clamp-3  lg:top-[13vw] lg:text-[1.4vw] lg:w-[15vw] md:top-[15vw] md:text-[2vw] md:w-[21vw] border-black">{food.user_bio}</p>
+                      <p className="absolute text-center 2xl:top-[13.7vw] 2xl:text-[1vw] font-times lg:text-[2vw] lg:top-[19vw] md:text-[2.5vw] md:top-[24vw]">1000</p>
+                      <AddButton/>
+                    </div>
+                  )}
+              </div>
           </div>  
       </>
     )
