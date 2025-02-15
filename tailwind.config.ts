7e3688abb464bf0a4826b03f878947aa0a52e07f
The-Yearly <theyearlyclub@gmail.com>
2025-02-15 19:43:22 +0530

    added more responsive ness aaaahhhhhhhhh

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,10 +4,14 @@ export default {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}", 
   ],
   theme: {
     extend: {
+      screens:{
+        'sm':'600px',
+        'xl-phone':'425px'
+      },
       colors: {
         background:  "var(--background)",
         foreground: "var(--foreground)",
